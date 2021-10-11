042342d2c57817a18bee9d4a69247181e638249d
Axelle Donot <axelle.donot@etu.umontpellier.fr>
2021-10-11 22:56:21 +0200

    TD4 exo4

--- TD4/Controller/ControllerVoiture.php
+++ TD4/Controller/ControllerVoiture.php
@@ -1,5 +1,9 @@
 <?php
 require_once ('../Model/ModelVoiture.php'); // chargement du modèle
-$tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
-require ('../View/Voiture/Liste.php');  //redirige vers la vue
+class ControllerVoiture {
+    public static function readAll() {
+        $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
+        require ('../View/Voiture/Liste.php');  //"redirige" vers la vue
+    }
+}
 ?>
