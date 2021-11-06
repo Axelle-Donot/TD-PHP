780381f24b0bdaa1c54380487bd9b302efae9494
Axelle Donot <axelle.donot@etu.umontpellier.fr>
2021-11-06 11:09:27 +0100

    EXO 1 TD6

--- TD5/Controller/routeur.php
+++ TD5/Controller/routeur.php
@@ -1,8 +1,14 @@
 <?php
 
 require_once File::build_path(array("Controller","ControllerVoiture.php"));
-// On recupère l'action passée dans l'URL
-$action = $_GET['action'];
+// On recupère l'action passée dans l'URL    
+//$action = $_GET['action'];
 // Appel de la méthode statique $action de ControllerVoiture
-ControllerVoiture::$action();
+if (isset($_GET['action'])){
+    ControllerVoiture::$action();
+}else {
+    ControllerVoiture::readAll();
+}
+
+
 ?>
